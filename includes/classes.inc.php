a61854aae5f83e34ed0a259bc727fea936ce0c39
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-20 17:51:53 +0800

    Update classes.inc.php

--- includes/classes.inc.php
+++ includes/classes.inc.php
@@ -9,7 +9,6 @@ function autoloader($class)
     $pathContorller = 'classes/controller/' . $class . ".class.php";
     $patView = 'classes/view/' . $class . ".class.php";
     $pathModel = 'classes/model/' . $class . ".class.php";
-    $pathLibs = 'classes/libs/' . $class . ".class.php";
 
     if (file_exists($pathContorller)) {
         require_once $pathContorller;
@@ -17,23 +16,17 @@ function autoloader($class)
         require_once $patView;
     } elseif (file_exists($pathModel)) {
         require_once $pathModel;
-    } elseif (file_exists($pathLibs)) {
-        require_once $pathLibs;
     }
 
     $pathContorller = '../classes/controller/' . $class . ".class.php";
     $patView = '../classes/view/' . $class . ".class.php";
     $pathModel = '../classes/model/' . $class . ".class.php";
-    $pathLibs = '../classes/libs/' . $class . ".class.php";
-
+  
     if (file_exists($pathContorller)) {
         require_once $pathContorller;
     } elseif (file_exists($patView)) {
         require_once $patView;
     } elseif (file_exists($pathModel)) {
         require_once $pathModel;
-    } elseif (file_exists($pathLibs)) {
-        require_once $pathLibs;
-    }
-
+    } 
 }
