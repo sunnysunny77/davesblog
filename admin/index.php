7311f02a3466806d6a6cfb99992050c257cb1945
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-22 10:13:38 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,7 @@ if (!$_SESSION["loggedin"]) {
     exit();
 }
 if ($_SESSION["loggedin"] && isset($_GET["action"]) && $_GET["action"] == "logout") {
+    
     session_destroy();
     header('Location: ./');
     exit();
