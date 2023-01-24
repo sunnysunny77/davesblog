fd2aa82090b2ef1d4bb4b57eb1d1e220b5e05dc4
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-01-24 10:48:51 +0800

    Update head.html.php

--- components/head.html.php
+++ components/head.html.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title><?php echo $this->title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $this->root; ?>style/main.css">
 </head>
 <body>
