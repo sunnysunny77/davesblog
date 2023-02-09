34a3534f94f286b7410fd51ab0b70cf534bda02c
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-02-09 20:28:42 +0800

    up

--- components/loginform.html.php
+++ components/loginform.html.php
@@ -1,11 +1,11 @@
 <form id="login" action="?action=login" method="post">
 
     <label> Username 
-        <input  type="text" name="username" value=""  />
+        <input  type="text" name="username" value="">
     </label>
 
     <label> Password 
-        <input type="password" name="password" value=""  />
+        <input type="password" name="password" value="">
     </label>
 
     <input type="submit" name="submit" value="Login">
