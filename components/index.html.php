7ad4360fa1e6b529f01b269078b32903242326d7
daniel <contractor@lateralaspect.com.au>
2023-10-26 21:40:01 +0800

    Update index.html.php

--- components/index.html.php
+++ components/index.html.php
@@ -14,11 +14,13 @@
 
     foreach ($this->result[isset($_GET["page"]) ? $_GET["page"] : 0] as $row) {
         
-        echo '<article class="mb-3 p-3">';
-        echo '<h2><a href="?action=viewpost&id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h2>';
+        echo '<article class="mb-3 p-3 d-flex flex-column justify-content-between">';
         echo '<div>';
+        echo '<h2><a href="?action=viewpost&id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h2>';
         echo '<p>Posted on ' . date('jS M Y H:i:s', strtotime($row['postDate'])) . '</p>';
         echo '<img width="50" height="50" alt="' . pathinfo($row["filename"], PATHINFO_FILENAME)  . '" src="data:' . $row["mimetype"] . ';base64,' . base64_encode($row['filedata']) . '" >';
+        echo '</div>';
+        echo '<div>';
         echo '<p class="p-md-3">' . $row['postDesc'] . '</p>';
         echo '<a href="?action=viewpost&id=' . $row['postID'] . '">Read Article</a>';
         echo '</div>';
