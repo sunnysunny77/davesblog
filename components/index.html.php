2f58b6c3168b0f5806573fc85d0c12a338dd0786
daniel <contractor@lateralaspect.com.au>
2023-10-27 06:40:42 +0800

    up

--- components/index.html.php
+++ components/index.html.php
@@ -19,11 +19,9 @@
         echo '<h2><a href="?action=viewpost&id=' . $row['postID'] . '">' . $row['postTitle'] . '</a></h2>';
         echo '<p>Posted on ' . date('jS M Y H:i:s', strtotime($row['postDate'])) . '</p>';
         echo '<img width="50" height="50" alt="' . pathinfo($row["filename"], PATHINFO_FILENAME)  . '" src="data:' . $row["mimetype"] . ';base64,' . base64_encode($row['filedata']) . '" >';
-        echo '</div>';
-        echo '<div>';
         echo '<p class="p-md-3">' . $row['postDesc'] . '</p>';
-        echo '<a href="?action=viewpost&id=' . $row['postID'] . '">Read Article</a>';
         echo '</div>';
+        echo '<a href="?action=viewpost&id=' . $row['postID'] . '">Read Article</a>';
         echo '</article>';
     }
     ?>
