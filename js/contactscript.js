19a68336b3e60281b1cd7d290c67bec80a228fc9
daniel <contractor@lateralaspect.com.au>
2024-07-26 08:35:51 +0800

    up

--- js/contactscript.js
+++ js/contactscript.js
@@ -3,25 +3,22 @@ document.querySelector("#contact").addEventListener("submit", (e) => {
   const formData = new FormData(e.currentTarget);
   const sent = document.querySelector("#sent");
 
-  let promise = new Promise(function(myResolve, myReject) {
-    let req = new XMLHttpRequest();
-    req.open("POST", "cont.php", true);
-    req.addEventListener("load", function() {
-      if (req.status === 200) {
-        myResolve(req.responseText);
-      } else {
-        myReject("File not Found");
-      }
-    });
-    req.send(formData);
-  });
-  promise.then((value) => {
+  fetch(
+    "./cont.php",
+    {
+      method: "POST",
+      body: formData
+    }
+  ).then((res) => {
+    if (!res.ok) {
+      throw new Error("File not Found");
+    }
+    return res.text();
+  }).then((value) => {
     sent.innerHTML = value;
-  }
-  ).catch((error) => {
+  }).catch((error) => {
     sent.innerHTML = error;
-  }
-  );
+  });
 
   e.preventDefault();
 },null);
