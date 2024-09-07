636f5b216573d49e27ec36e12baa47d6e396700a
daniel <contractor@lateralaspect.com.au>
2024-09-07 08:50:30 +0800

    up

--- js/preload.js
+++ js/preload.js
@@ -4,19 +4,13 @@ export const preload = () => {
 
   const path = location.pathname;
 
-  if(path.includes("admin")) {
 
     preload_image("../images/atom.svg");
-  } else if(path.includes("about")) {
+  if(path.includes("about")) {
 
     preload_image("./images/about.png");
-    preload_image("./images/atom.svg");
   } else if(path.includes("contact")) {
     
     preload_image("./images/contact.png");
-    preload_image("./images/atom.svg");
-  } else {
-    
-    preload_image("./images/atom.svg");
-  }
+  } 
 };
