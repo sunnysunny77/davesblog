411736cd4f49cfd8b14043549fc5a1f29ee39cb6
daniel <contractor@lateralaspect.com.au>
2024-09-09 11:22:50 +0800

    preload

--- site/js/preload.js
+++ site/js/preload.js
@@ -1,14 +1,47 @@
-const preload_image = (url) => {
-  var img = new Image();
-  img.src = url;
+const preload = (href, as) => {
+  const preloadLink = document.createElement("link");
+  preloadLink.href = href;
+  preloadLink.rel = "preload";
+  preloadLink.as = as;
+  if (as === "font") {
+    preloadLink.crossOrigin = "true";
+  }
+  document.head.appendChild(preloadLink);
 };
 
 const path = location.pathname;
 
+
+const fonts = [
+  "font/OpenSans.ttf",
+  "webfonts/fa-brands-400.woff2",
+  "webfonts/fa-solid-900.woff2"
+];
+
 if(path.includes("about")) {
 
-  preload_image("./images/about.webp");
-} else if(path.includes("contact")) {
+  for (const index of fonts) {
+
+    preload(`./${index}`, "font");
+  }
+  preload("./images/about.webp", "image");
+} else if (path.includes("contact")) {
   
-  preload_image("./images/contact.webp");
-} 
+  for (const index of fonts) {
+
+    preload(`./${index}`, "font");
+  }
+  preload("./images/contact.webp", "image");
+} else if (path.includes("admin")) {
+  
+  for (const index of fonts) {
+
+    preload(`../${index}`, "font");
+  }
+} else {
+
+  for (const index of fonts) {
+
+    preload(`./${index}`, "font");
+  }
+}
