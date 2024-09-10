3621eea80de8352dcccf94df591cdb90220a715e
daniel <contractor@lateralaspect.com.au>
2024-09-10 09:56:33 +0800

    Update worker.js

--- site/worker.js
+++ site/worker.js
@@ -1,37 +1,63 @@
-self.addEventListener("install", function(event) {
+const version = 1;
+const cacheName = `davesblog-v${version}`;
+
+const cacheAssets = [
+  "./",
+  "./index.php",
+  "./about.php",
+  "./contact.php",
+  "./admin/index.php",
+  "./css/app.min.css",
+  "./js/app.min.js",
+  "./font/OpenSans.ttf",
+  "./webfonts/fa-brands-400.woff2",
+  "./webfonts/fa-solid-900.woff2",
+  "./images/about.webp",
+  "./images/contact.webp",
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service worker is installed");
 
   event.waitUntil(
-    caches.open("v1").then(function(cache) {
-      return cache.addAll([
-        "./",
-        "./index.php",
-        "./about.php",
-        "./contact.php",
-        "./admin/index.php",
-        "./css/app.min.css",
-        "./js/app.min.js",
-        "./font/OpenSans.ttf",
-        "./webfonts/fa-brands-400.woff2",
-        "./webfonts/fa-solid-900.woff2",
-        "./images/about.webp",
-        "./images/contact.webp",
-      ]);
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Caching assets");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker is activated");
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return cacheNames.map((cache) => {
+        if (cache !== cacheName) {
+          console.log("Clearing old caches");
+          caches.delete(cache);
+        }
+      });
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching via Service worker");
 
-  event.respondWith(caches.open("v1").then((cache) => {
-
-      return fetch(event.request).then((networkResponse) => {
-
-        cache.put(event.request, networkResponse.clone());
-
-        return networkResponse;
-      }).catch(() => {
-
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
         return caches.match(event.request);
-    } );
-  }));
+      })
+  );
 });
