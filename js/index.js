0a2c54304f2e1ec51e75e6ef08ba5366ac715fb6
daniel <contractor@lateralaspect.com.au>
2024-09-12 20:40:32 +0800

    up

--- js/index.js
+++ js/index.js
@@ -16,9 +16,11 @@ import { deleteuserscript } from "./deleteuserscript.js";
 import { deletescript } from "./deletescript.js";
 import { contactscript } from "./contactscript.js";
 import { top } from "./top.js";
+import { fallback } from "./fallback.js";
 
 events(window, "load", () => {
 
+    fallback();
     top();
     contactscript();
     deletescript();
