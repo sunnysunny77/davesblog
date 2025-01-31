55cfb408f8e4c06490474a1df31917671bcd79cb
daniel <contractor@lateralaspect.com.au>
2025-01-31 16:16:48 +0800

    Update session.inc.php

--- site/includes/session.inc.php
+++ site/includes/session.inc.php
@@ -1,5 +1,5 @@
 <?php 
 ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 0);
+ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_samesite', "Strict");
 session_start();
